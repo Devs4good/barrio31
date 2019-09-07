428eebc5baaec1c305a7ab4e4bd32748ab3cc51d
Agustin Rojas <your_email@abc.com>
2019-09-07 15:47:04 -0300

    Add place constructor

--- backend/barrio31/entities/place.py
+++ backend/barrio31/entities/place.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, Sequence, JSON, Float, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, String, JSON, DateTime
 from backend.barrio31.database.data_base import DbEntity
 
 
@@ -11,6 +11,12 @@ class Place(DbEntity):
     coordenates = Column(JSON)
     category = Column(String(200))
     creation_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, name, coordenates, category):
+        self.id = str(uuid.uuid1())
+        self.name = name
+        self.coordenates = coordenates
+        self.category = category
 
     @classmethod
     def get(cls, id=None):
